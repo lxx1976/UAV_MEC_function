d526fe68d66f0c2fa8bc5753239d88ac24cbc90a
lxx1976 <139452426+lxx1976@users.noreply.github.com>
2026-03-03 16:35:28 +0800

    Add files via upload

---
+++ algorithms/utils/util.py
@@ -0,0 +1,17 @@
+import copy
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)#初始化权重的一种平衡方法，用来赋予初值
+    bias_init(module.bias.data)  #偏置（bias）
+    return module
+
+def get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+def check(input):
+    output = torch.from_numpy(input) if type(input) == np.ndarray else input
+    return output
